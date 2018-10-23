2a8a4a0105a755cc00a9e0a615d01bd2bf9e0816
Nicholas Jozefczyk <nickjay@Nicholass-MacBook-Air.local>
2018-10-23 16:54:28 -0700

    Updates

---
+++ precache-manifest.635d2da22763ae4f26c53d13464030a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a70c675f9a23daa38fda",
+    "url": "/clickygame/static/js/runtime~main.a70c675f.js"
+  },
+  {
+    "revision": "59e0bce87fb93070ce4f",
+    "url": "/clickygame/static/js/main.59e0bce8.chunk.js"
+  },
+  {
+    "revision": "709840df12e4494d4644",
+    "url": "/clickygame/static/js/1.709840df.chunk.js"
+  },
+  {
+    "revision": "59e0bce87fb93070ce4f",
+    "url": "/clickygame/static/css/main.14e398d9.chunk.css"
+  },
+  {
+    "revision": "0b7eeb65ac629cc53c196ce700a20784",
+    "url": "/clickygame/index.html"
+  }
+];
